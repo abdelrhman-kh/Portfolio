45a45539c853ca6739d1eca4b28a7a518bfc72e1
a-khamis01 <a.khamis@meemdev.com>
2024-07-06 23:29:36 +0300

    add portfolio website files

---
+++ public/js/contact.js
@@ -0,0 +1,45 @@
+function toggleMenu() {
+    var navLinks = document.getElementById("nav-links");
+    navLinks.classList.toggle("active");
+}
+
+$(document).ready(function() {
+    // Smooth scroll
+    $('nav ul li a').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    // Animate on scroll
+    $('.experience-item').waypoint(function(direction) {
+        $(this.element).addClass('animate__animated animate__fadeInUp');
+    }, {
+        offset: '75%'
+    });
+
+    // Tooltips
+    tippy('[data-tippy-content]');
+
+    // Back to top button
+    var backToTopButton = $('#back-to-top');
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 1) {
+            backToTopButton.addClass('show');
+        } else {
+            backToTopButton.removeClass('show');
+        }
+    });
+
+    backToTopButton.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop: 0}, '300');
+    });
+});
---
+++ public/js/main.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+    var navLinks = document.getElementById("nav-links");
+    navLinks.classList.toggle("active");
+}
